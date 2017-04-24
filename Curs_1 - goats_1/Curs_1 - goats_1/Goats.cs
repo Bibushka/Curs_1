bbcd1102b230c9dc3b3e735cb00f7071f47a8710
Bibushka <bianca_bucur03@yahoo.com>
2017-04-24 21:13:49 +0300

    Calculate kilograms of hay eaten by a number of goats in a number of days.

---
+++ Curs_1 - goats_1/Curs_1 - goats_1/Goats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Curs_1___goats_1
+{
+    [TestClass]
+    public class Goats_1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int kilos = GetKilograms(1, 1, 1, 1, 2);
+            Assert.AreEqual(2, kilos);
+        }
+
+        public int GetKilograms(int initialNumberOfDays, int initialNumberOfGoats,  int initialKilogramsOfHay, int numberOfDays, int numberOfGoats)
+        {
+            return numberOfDays * numberOfGoats * initialKilogramsOfHay / initialNumberOfDays / initialNumberOfGoats;
+        }
+    }
+}
