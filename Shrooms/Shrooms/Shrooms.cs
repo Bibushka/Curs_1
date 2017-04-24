d7899617a9e45e455f3a2fe416523cf653845c5a
Bibushka <bianca_bucur03@yahoo.com>
2017-04-24 21:56:53 +0300

    The variable "whites" has been eliminated

---
+++ Shrooms/Shrooms/Shrooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shrooms
+{
+    [TestClass]
+    public class Shrooms
+    {
+        [TestMethod]
+        public void FirstSpring()
+        {
+            int numberOfRedMushrooms = GetNumberOfMushrooms(3, 2);
+            Assert.AreEqual(2, numberOfRedMushrooms);
+        }
+
+        public int GetNumberOfMushrooms(int total, int x)
+        {
+            int reds = x * total / (x + 1);
+            return reds;
+        }
+    }
+}
