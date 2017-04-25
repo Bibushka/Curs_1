5ac318c017c387e46d872dbd9c8662fde87058f9
Bibushka <bianca_bucur03@yahoo.com>
2017-04-25 22:03:25 +0300

    Pavement test with JuniorMind values and my result

--- Pavements/Pavements/Pavements.cs
+++ Pavements/Pavements/Pavements.cs
@@ -6,11 +6,11 @@ namespace Pavements
     [TestClass]
     public class Pavements
     {
-        [TestMethod]     
-        public void JuniorMindResult()
+        [TestMethod]
+        public void MyResult()
         {
             double numberOfSlabs = GetNumberOfSlabs(6, 6, 4, 4);
-            Assert.AreEqual(4, numberOfSlabs);
+            Assert.AreEqual(3, numberOfSlabs);
         }
 
         public double GetNumberOfSlabs(double marketLenght, double marketWidth, double slabLenght, double slabWidth)
