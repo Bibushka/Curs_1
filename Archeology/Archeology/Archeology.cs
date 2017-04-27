db2d3fb0fac629b681f5610d594046af2d2e0599
Bibushka <bianca_bucur03@yahoo.com>
2017-04-27 22:58:48 +0300

    BuildingSurface test: failure ????
    Da eroare: expected value 0.5, actual 0.5.

---
+++ Archeology/Archeology/Archeology.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archeology
+{
+    [TestClass]
+    public class Archeology
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double buildingSurface = CalculateBuildingSurface(0, 0, 1, 0, 0, 1);
+            Assert.AreEqual(0.5, buildingSurface);
+        }
+
+        public double CalculateBuildingSurface(double xA, double yA, double xB, double yB, double xC, double yC)
+        {
+            double sideOne = Math.Sqrt(Math.Pow((xA - xB), 2) + Math.Pow((yA - yB), 2));
+            double sideTwo = Math.Sqrt(Math.Pow((xB - xC), 2) + Math.Pow((yB - yC), 2));
+            double sideThree = Math.Sqrt(Math.Pow((xC - xA), 2) + Math.Pow((yC - yA), 2));
+            double semiPerimetre = (sideOne + sideTwo + sideThree) / 2;
+            double buildingSurface = Math.Sqrt(semiPerimetre * (semiPerimetre - sideOne) * (semiPerimetre - sideTwo) * (semiPerimetre - sideThree));
+            return buildingSurface;
+        }
+    }
+}
