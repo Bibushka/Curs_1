8b1563a0c01c0bdb6f097e31625da921481cbdd2
Bibushka <bianca_bucur03@yahoo.com>
2017-04-27 21:41:47 +0300

    Train test failure: result 0???

---
+++ Trains/Trains/Trains.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trains
+{
+    [TestClass]
+    public class Trains
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double birdDistance = GetBirdDistance(4, 1);
+            Assert.AreEqual(2, birdDistance);
+        }
+
+        public double GetBirdDistance(double totalDistance, double trainSpeed)
+        {
+            double birdTime = 1 / 4 * totalDistance / trainSpeed;
+            double birdDistance = birdTime * 2 * trainSpeed;
+            return birdDistance;
+        }
+    }
+}
